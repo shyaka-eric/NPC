a6d0cd55133a33336c158bdecf65437a2b3ad7de
shyaka-eric <kezafatman@gmail.com>
2025-06-23 14:28:14 +0200

    new database using postgres

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -13,6 +13,7 @@ import dj_database_url
 from pathlib import Path
 import os
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,14 +22,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-CHANGE_THIS_TO_A_RANDOM_SECRET_KEY')  
+SECRET_KEY = os.environ.get('SECRET_KEY')  
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'           
 
 # Superuser configuration
 SUPERUSER_PASSWORD = os.environ.get('SUPERUSER_PASSWORD', 'admin123')
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'logistics-backend-qh1y.onrender.com,localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
 
 
 # Application definition
@@ -94,8 +95,8 @@ DATABASES = {
 }
 
 # Comment out dj_database_url for local development
-# database_url = os.environ.get('DATABASE_URL')
-# DATABASES['default'] = dj_database_url.parse(database_url)
+database_url = os.environ.get('DATABASE_URL')
+DATABASES['default'] = dj_database_url.parse(database_url)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
